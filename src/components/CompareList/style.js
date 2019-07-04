f7ec189b520e347bbc9e70ece0638fdb9a347bcd
Dieggo Phillippe Sousa Carrilho <dieggo.carrilho@selfitacademias.com.br>
2019-07-03 22:29:04 -0300

    Treino de React

---
+++ src/components/CompareList/style.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: center;
+margin-top: 50px;
+`;
+export const Repository = styled.div`
+width: 250px;
+background: #FFF;
+border-radius: 3px;
+margin: 0 10px;
+
+display: flex;
+flex-direction: column;
+  header {
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  }
+  strong {
+  font-size: 24px;
+  margin-top: 10px;
+  }
+  small {
+  font-size: 14px;
+  color: #666
+  }
+  img {
+  width: 64px;
+  }
+  
+  ul {
+    list-style: none;
+    li {
+      font-weight: bold;
+      padding: 12px 20px;
+      small {
+        font-weight: normal;
+        color: #999;
+        font-style: italic;
+        font-size: 12px
+      }
+      &:nth-child(2n-1) {
+        background: #F5F5F5;
+      }
+    }
+  }
+`;
